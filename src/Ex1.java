59b287aa5ceaa2abdab56fcf8f7fa8a6b3ad87d6
Ivan Bosch <ivan.bosch.amengual@gmail.com>
2021-01-13 13:09:47 +0100

    Update

--- src/Ex1.java
+++ src/Ex1.java
@@ -10,15 +10,14 @@ import java.util.ArrayList;
 public class Ex1 {
 
     public static void main(String[] args) {
-        ArrayList<String> companys = new ArrayList<String>();
+        ArrayList<String> companys = new ArrayList<>();
         companys.add("Adán");
         companys.add("Joshua");
         companys.add("Diego");
         companys.add("Nico");
         companys.add("Miguel");
         companys.add("Ismael");
-        for (int i = 0; i < companys.size(); i++) {
-            System.out.println(companys.get(i));
+
+        System.out.println(companys.toString());
         }
     }
-}
